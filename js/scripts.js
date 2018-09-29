fbe1d1cc3a97e3d55520655af367971f461021cc
Monica Gibbs <gibbsmonica@gmail.com>
2018-09-28 23:55:00 -0400

    Added jquery classes

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,8 @@
 // JavaScript Document
-console.log("Explore my portfolio")
+console.log("Explore my portfolio");
+
+$(document).ready(function() {
+  $(".mg_preview").on("click", function() {
+    $(this).siblings(".mg_details").slideToggle();
+    });
+});
