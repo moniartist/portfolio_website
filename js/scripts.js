2e717451111f274c253d58fef6b2290f8bb98ef5
Monica Gibbs <gibbsmonica@gmail.com>
2018-10-09 20:32:17 -0400

    Added new styling cleaned code

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 console.log("Explore my portfolio");
 
 $(document).ready(function() {
+	"use strict";
   $(".mg-preview").on("click", function() {
     $(this).siblings(".mg-details").slideToggle();
     });
